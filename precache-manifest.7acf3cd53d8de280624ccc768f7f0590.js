d3cebdf517a7bf071a1d40724bf12f6a3d47d86e
fimorr <fimorrr@yandex.ru>
2020-02-01 20:40:33 +0300

    Updates

---
+++ precache-manifest.7acf3cd53d8de280624ccc768f7f0590.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf1ac67aa0f879522d03eb90495d0b0",
+    "url": "/mol-res-center/index.html"
+  },
+  {
+    "revision": "df69139ce3b41550fd8c",
+    "url": "/mol-res-center/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "076a54e4eaeda2156a7c",
+    "url": "/mol-res-center/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "df69139ce3b41550fd8c",
+    "url": "/mol-res-center/static/js/2.254b2977.chunk.js"
+  },
+  {
+    "revision": "076a54e4eaeda2156a7c",
+    "url": "/mol-res-center/static/js/main.278c20a5.chunk.js"
+  },
+  {
+    "revision": "a99b7ddf320e19747a74",
+    "url": "/mol-res-center/static/js/runtime-main.956a84c3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/mol-res-center/static/media/persik.4e1ec840.png"
+  }
+]);
